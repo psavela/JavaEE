99f708f24482d3eed3044ee7ce01ccdbd500f0ff
psavela <petri.m.savela@gmail.com>
2016-02-10 10:23:05 +0200

    First draft

---
+++ SpringMVC/MavenBase/src/main/java/com/base/controller/DefaultController.java
@@ -0,0 +1,27 @@
+
+package com.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DefaultController {
+    
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String index(ModelMap map){
+        //Define attributes you want to use in your template index.jsp
+        map.addAttribute("name", "psavela");
+        
+        return "index";
+    } 
+    
+    @RequestMapping(value="/second", method=RequestMethod.GET)
+    public String second(ModelMap map){
+        //Render second.jsp      
+        return "second";
+    }     
+    
+    
+}
